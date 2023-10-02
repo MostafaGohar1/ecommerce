26f8c0a11786301760a64e48df530f706889cd16
unknown <mostafagoherr@icloud.com>
2023-10-02 09:32:42 +0300

    most-sold-product

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.js");
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/index.css";
 import "@fortawesome/fontawesome-free/js/all";
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl);
+
+document
+  .querySelectorAll('[data-bs-toggle="tooltip"]')
+  .forEach((item) => new bootstrap.Tooltip(item));
+
+document.querySelectorAll(".add-to-cart-btn").forEach((item) => {
+  item.addEventListener("click", () => {
+    alert("أضيف المنتج إلى عربة الشراء؟");
+  });
 });
